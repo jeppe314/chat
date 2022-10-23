d3a3f89ad9311523f3ff5d11ce3cc4db3d4c96bc
jeppe314 <jesperlindqvist93@gmail.com>
2022-10-23 18:33:13 +0200

    Title styles

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -4,9 +4,9 @@ export const Login = () => {
   return (
     <div className="loginWrapper">
       <div className="titleWrapper">
+        <h1 className="overlay">FireChat</h1>
         <h1 className="appTitle">FireChat</h1>
         <h1 className="shadow">FireChat</h1>
-        <h1 className="behind">FireChat</h1>
       </div>
       <button className="loginBtn">Sign in with google</button>
     </div>
