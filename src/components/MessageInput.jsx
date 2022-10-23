9111516a86940c8095c014c46017ece296d9efee
jeppe314 <jesperlindqvist93@gmail.com>
2022-10-23 19:29:13 +0200

    Style message input

--- src/components/MessageInput.jsx
+++ src/components/MessageInput.jsx
@@ -1,8 +1,10 @@
 import React from "react"
 
 export const MessageInput = () => {
-  return <div className="messageInput">
-    <input type="text" />
-    <button className="sendBtn">Send</button>
-  </div>
+  return (
+    <div className="messageInput">
+      <textarea spellCheck="false" placeholder="Say hi!" />
+      <button className="sendBtn">Send</button>
+    </div>
+  )
 }
