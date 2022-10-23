bee5d48dd832ce6e6d66f3a4355d1e4c45b85cb5
jeppe314 <jesperlindqvist93@gmail.com>
2022-10-23 11:25:54 +0200

    Basic sidebarNav

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,5 +1,12 @@
 import React from "react"
+import { SidebarChats } from "./SidebarChats"
+import { SidebarNav } from "./SidebarNav"
 
 export const Sidebar = () => {
-  return <div className="sidebar">Sidebar</div>
+  return (
+    <div className="sidebar">
+      <SidebarNav />
+      <SidebarChats />
+    </div>
+  )
 }
