c3d08ea7e2bfe5abb3a3156e365ba845ef2aed51
jeppe314 <jesperlindqvist93@gmail.com>
2022-10-23 17:43:52 +0200

    Title logo css

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,8 +1,14 @@
 import React from "react"
 
 export const Login = () => {
-  return <div className="loginWrapper">
-    <h1 className="appTitle">FireChat</h1>
-    <button className="loginBtn">Sign in with google</button>
-  </div>
+  return (
+    <div className="loginWrapper">
+      <div className="titleWrapper">
+        <h1 className="appTitle">FireChat</h1>
+        <h1 className="shadow">FireChat</h1>
+        <h1 className="behind">FireChat</h1>
+      </div>
+      <button className="loginBtn">Sign in with google</button>
+    </div>
+  )
 }
