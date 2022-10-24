a78b3f2af2f97f5a97fc8109dbb770633df0b8ad
jeppe314 <jesperlindqvist93@gmail.com>
2022-10-24 20:23:15 +0200

    Set profile img to users

--- src/components/SidebarNav.jsx
+++ src/components/SidebarNav.jsx
@@ -7,17 +7,16 @@ import { AuthContext } from "../context/AuthContext"
 export const SidebarNav = () => {
   const { currentUser } = useContext(AuthContext)
 
+  const { displayName, photoURL } = currentUser
+
   return (
     <div className="sidebarNav">
       <div className="sidebarNavLogo">
         <ImFire className="fireLogo" />
       </div>
       <div className="sidebarNavRight">
-        <img
-          src="https://images.unsplash.com/photo-1570295999919-56ceb5ecca61?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80"
-          alt=""
-        />
-        <span>{currentUser.email}</span>
+        <img src={photoURL} alt="" />
+        <span>{displayName}</span>
         <button className="logoutBtn" onClick={() => signOut(auth)}>
           Logout
         </button>
