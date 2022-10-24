36577a0513a27d96472871cdfa3a2338c3c499db
jeppe314 <jesperlindqvist93@gmail.com>
2022-10-24 16:18:01 +0200

    Started working on auth

---
+++ src/components/FireTitle.jsx
@@ -0,0 +1,11 @@
+import React from "react"
+
+export const FireTitle = () => {
+  return (
+    <div className="titleWrapper">
+      <h1 className="overlay">FireChat</h1>
+      <h1 className="appTitle">FireChat</h1>
+      <h1 className="shadow">FireChat</h1>
+    </div>
+  )
+}
--- src/components/RegisterForm.jsx
+++ src/components/RegisterForm.jsx
@@ -1,16 +1,41 @@
-import React from "react"
+import React, { useRef } from "react"
 import { Link } from "react-router-dom"
+import { createUserWithEmailAndPassword } from "firebase/auth"
+import { auth } from "../firebase"
 
 export const RegisterForm = () => {
+  const emailRef = useRef()
+  const passwordRef = useRef()
+  const usernameRef = useRef()
+
+  const email = emailRef.current.value
+  const username = usernameRef.current.value
+  const password = passwordRef.current.value
+
+  const handleSubmit = (e) => {
+    createUserWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        // Signed in
+        const user = userCredential.user
+        // ...
+      })
+      .catch((error) => {
+        const errorCode = error.code
+        const errorMessage = error.message
+        // ..
+      })
+  }
+
   return (
-    <form className="form">
-      <input type="text" className="username" placeholder="Username" />
-      <input type="text" className="email" placeholder="Email" />
+    <form className="form" onSubmit={() => handleSubmit}>
+      <input type="text" className="username" ref={usernameRef} placeholder="Username" />
+      <input type="text" className="email" ref={emailRef} placeholder="Email" />
       <input
         type="password"
         pattern=".{8,}"
         title="Password needs to be minumum 8 characters long"
         className="password"
+        ref={passwordRef}
         placeholder="Password"
       />
       <button className="btn registerBtn">Sign up</button>
