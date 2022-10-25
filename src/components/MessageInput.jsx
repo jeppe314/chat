0351afedb7da409c57bb5c38a8fe78d4b4c01c69
jeppe314 <jesperlindqvist93@gmail.com>
2022-10-25 22:02:14 +0200

    More css

--- src/components/MessageInput.jsx
+++ src/components/MessageInput.jsx
@@ -1,10 +1,13 @@
 import React from "react"
+import { FiSend } from "react-icons/fi"
 
 export const MessageInput = () => {
   return (
     <div className="messageInput">
       <textarea spellCheck="false" placeholder="Say hi!" />
-      <button className="sendBtn">Send</button>
+      <button className="sendBtn">
+        <FiSend />
+      </button>
     </div>
   )
 }
