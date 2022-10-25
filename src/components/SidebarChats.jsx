0cb26a35fb4cb9a7ddd42118d2c05847aca19689
jeppe314 <jesperlindqvist93@gmail.com>
2022-10-25 19:55:13 +0200

    added useeffect to sidebarchats to fetch all chats

--- src/components/SidebarChats.jsx
+++ src/components/SidebarChats.jsx
@@ -1,7 +1,31 @@
-import React from "react"
+import React, { useContext, useState, useEffect } from "react"
 import { User } from "./User"
+import { AuthContext } from "../context/AuthContext"
+import { db } from "../firebase"
+import { doc, getDoc, onSnapshot } from "firebase/firestore"
 
 export const SidebarChats = () => {
+  const { currentUser } = useContext(AuthContext)
+
+  const [chats, setChats] = useState([])
+
+  useEffect(() => {
+    //gets the current users chats from db
+    const getChats = () => {
+      const unsub = onSnapshot(doc(db, "userChats", currentUser.uid), (doc) => {
+        setChats(doc.data())
+      })
+
+      // cleanup
+      return () => {
+        unsub()
+      }
+    }
+    //call function above only if there is a current user (otherwise it crashes)
+    currentUser.uid && getChats()
+  }, [currentUser.uid])
+
+  console.log(chats)
   return (
     <div className="chats">
       <User />
