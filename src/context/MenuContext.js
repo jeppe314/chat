662928911450abddb1bc30cd05b47ad24d2d6048
jeppe314 <jesperlindqvist93@gmail.com>
2022-10-27 19:22:09 +0200

    Created context for burger menu

---
+++ src/context/MenuContext.js
@@ -0,0 +1,9 @@
+import React, { useState, createContext } from "react"
+
+export const MenuContext = createContext()
+
+export const MenuContextProvider = ({ children }) => {
+  const [isOpen, setOpen] = useState(false)
+
+  return <MenuContext.Provider value={{ isOpen, setOpen }}>{children}</MenuContext.Provider>
+}
