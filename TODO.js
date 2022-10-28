a48aa02ac3aa3297def59bbb71e3d4f0c5af0485
jeppe314 <jesperlindqvist93@gmail.com>
2022-10-28 18:28:12 +0200

    Updated todo with todo highlights extension

---
+++ TODO.js
@@ -0,0 +1,31 @@
+// Add fire animation to title
+// Add Fire logo next to title
+
+// CHECK Make responsive (hamburger menu for tablet and smaller)
+
+// CHECK Add loading screen after login / register (chat renders before img has loaded, i.e.)
+
+// CHECK NEXT: Search function sidebar
+
+// TODO: Better looking speechbubbles
+// TODO: Search results animation
+
+// TODO: in future: Implement Algolia for better search function
+
+// TODO: Show earlier messages button top of chat
+
+// TODO: Open chat on searched user immediately so u dont have to click twice
+
+// FIXME: white screen after new registration, sidebarChats breaks
+
+// CHECK  Move sidebar to inside chat component, makes it easier to animate hamburger menu
+
+// TODO: Make not able to zoom in mobile ? Not sure if possible since web
+
+// FIXME: Small divs ? or sth next to users in sidebar in mobile wtf
+
+// FIXME: Viewport height thingy in mobile
+
+// TODO: Error msgs on signup or login
+
+//
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { useContext } from "react"
 import { RegisterForm } from "../components/RegisterForm"
 import { FireTitle } from "../components/FireTitle"
 import { LoadingScreen } from "../components/LoadingScreen"
